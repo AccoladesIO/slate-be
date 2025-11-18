9599578e350eb5b5eaf6112a60a94581d118204c
Ononobi Praise <ononobip@gmail.com>
2025-11-18 15:18:13 +0100

    fix for local development

--- src/config/database.ts
+++ src/config/database.ts
@@ -4,6 +4,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const sequelize = new Sequelize({
   dialect: "postgres",
   host: process.env.DB_HOST,
@@ -12,6 +14,14 @@ const sequelize = new Sequelize({
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT) || 5432,
   logging: false,
+  dialectOptions: isProduction
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // needed for Koyeb
+        },
+      }
+    : {}, // no SSL locally
 });
 
 export const connectDB = async () => {
